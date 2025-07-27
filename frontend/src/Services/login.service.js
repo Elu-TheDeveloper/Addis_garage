a8a2fa98aab2e3ebd7d3d1a094c540695be5bfcf
EluTheDeveloper <eladaba2022@gmail.com>
2025-07-27 12:20:31 +0300

    login

---
+++ frontend/src/Services/login.service.js
@@ -0,0 +1,16 @@
+const api_url = import.meta.env.VITE_API_URL;
+
+const logIn = async (formData) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  };
+
+  const response = await fetch(`${api_url}/api/employee/login`, requestOptions);
+  return response;
+};
+
+const loginService = { logIn };
+
+export default loginService;
